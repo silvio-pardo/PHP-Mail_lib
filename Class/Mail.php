cac726f10bd71dec71383b16c291d516c5c1c04e
Silvio <38363507+silviohw@users.noreply.github.com>
2019-10-10 22:09:50 +0200

    Update Mail.php

--- Class/Mail.php
+++ Class/Mail.php
@@ -4,6 +4,36 @@
  * User: Silvio
  * Date: 26/07/2019
  * Time: 14:17
+ * -------------------
+ * PHP-Mail_lib/Mail.php
+ *
+ * Sending E-mail in PHP with native php function
+ *
+ * PHP versions 4+
+ *
+ * Copyright (c) 2019 Silvio Pardo
+ *
+ * LICENSE:
+ *
+ * This library is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation;
+ * either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package    PHP-Mail_lib
+ * @author     Silvio Pardo <silviopardo@silviopardo.it>
+ * @copyright  2019 Silvio Pardo
+ * @license    http://www.opensource.org/licenses/lgpl-license.php LGPL
  */
 
 require 'Mail_Exception.php';
